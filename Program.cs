d0a39c0c913514cc0c44e33f35ad073b813a5051
Snegativr <93019177+Snegativr@users.noreply.github.com>
2023-10-08 14:49:34 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddTransient<CalcService>();
+builder.Services.AddTransient<TimeOfDayService>();
 var app = builder.Build();
-
 app.Run(async context =>
 {
-    Company company = new Company();
-    company.Name = "Name111";
-    company.Email = "Email222";
-    company.Address = "Address333";
+    var timeOfDayService = app.Services.GetService<TimeOfDayService>();
+    var CalcService = app.Services.GetService<CalcService>();
 
-    Random random = new Random();
-    int randomNumber = random.Next(0, 101);
-
-    await context.Response.WriteAsync($"Compamy:\n {company.Name} \n {company.Email} \n {company.Address} ");
-    await context.Response.WriteAsync($"\nRandom number: {randomNumber}");
+    await context.Response.WriteAsync($"Time: {timeOfDayService?.GetTimeOfDay()}");
+    await context.Response.WriteAsync($"\nSum of 77 and 33: {CalcService?.Add(77,33)}");
+    await context.Response.WriteAsync($"\nSubtraction of 77 and 33: {CalcService?.Subtract(77, 33)}");
+    await context.Response.WriteAsync($"\nDividion of 77 and 33: {CalcService?.Divide(77, 33)}");
+    await context.Response.WriteAsync($"\nMultiplication of 33 and 77: {CalcService?.Multiplication(33, 77)}");
 
 });
 app.Run();
-public class Company
-{
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Address { get; set; }
-}
